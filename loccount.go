d78f06c69fa0e59290edcede8f0a7cdeb13bb8d7
Eric S. Raymond <esr@thyrsus.com>
2016-11-12 04:41:52 -0500

    Implement exclusions.

--- loccount.go
+++ loccount.go
@@ -6,6 +6,8 @@ import "os"
 import "path/filepath"
 import "strings" 
 
+var exclusions []string
+
 // process - stub, eventually the statistics gatherer 
 func process(path string) {
 	fmt.Printf("%s\n", path)
@@ -14,10 +16,17 @@ func process(path string) {
 // filter - winnows out uninteresting paths before handing them to process
 func filter(path string, info os.FileInfo, err error) error {
 	//basename := filepath.Base(path)
+
 	/* ignore dotfiles due to version cmtrol systems */
 	if strings.Contains(path, "/.") || path[0] == '.' {
 		return err
 	}
+	for i :=range exclusions {
+		if path == exclusions[i] || strings.HasPrefix(path, exclusions[i] + "/") {
+			return err
+		}
+	}
+
 	process(path)
 	return err
 }
@@ -26,7 +35,7 @@ func main() {
 	excludePtr := flag.String("exclude", "", "directories to exclude")
 	flag.Parse()
 
-	fmt.Printf("excludes: %s\n", *excludePtr)
+	exclusions = strings.Split(*excludePtr, ",")
 	roots := flag.Args()
 	for i := range roots {
 		filepath.Walk(roots[i], filter)
