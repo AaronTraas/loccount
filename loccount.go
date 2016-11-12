6f43993fe9936855301c969a290437f9a0b2856b
Eric S. Raymond <esr@thyrsus.com>
2016-11-12 05:25:43 -0500

    Regularize some checks

--- loccount.go
+++ loccount.go
@@ -15,11 +15,24 @@ func process(path string) {
 
 // filter - winnows out uninteresting paths before handing them to process
 func filter(path string, info os.FileInfo, err error) error {
-	//basename := filepath.Base(path)
+	neverInterestingByPrefix := []string{"."}
+	neverInterestingByInfix := []string{".so.", "/."}
+	neverInterestingBySuffix := []string{"~", ".a", ".la", ".o", ".so"}
 
-	/* ignore dotfiles due to version cmtrol systems */
-	if strings.Contains(path, "/.") || path[0] == '.' {
-		return err
+	for i := range neverInterestingByPrefix {
+		if strings.HasSuffix(path, neverInterestingByPrefix[i]) {
+			return err
+		}
+	}
+	for i := range neverInterestingByInfix {
+		if strings.Contains(path, neverInterestingByInfix[i]) {
+			return err
+		}
+	}
+	for i := range neverInterestingBySuffix {
+		if strings.HasSuffix(path, neverInterestingBySuffix[i]) {
+			return err
+		}
 	}
 	for i := range exclusions {
 		if path == exclusions[i] || strings.HasPrefix(path, exclusions[i]+"/") {
