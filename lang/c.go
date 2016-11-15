813ea4d86ba1ea63f94a27c1004f6aecbf570b2f
Eric S. Raymond <esr@thyrsus.com>
2016-11-14 22:58:56 -0500

    First compilation of C handler.

--- lang/c.go
+++ lang/c.go
@@ -1,8 +1,198 @@
 package lang
 
+/*
+ * Algorithm by David A. Wheeler; Go code by ESR.
+ */
+
+import "os"
+import "strings"
+import "io"
+import "bufio"
+import "log"
 import "loccount/stats"
 
+/* Modes */
+const NORMAL = 0
+const INSTRING = 1
+const INCOMMENT = 2
+
+/* Types of comments: */
+const ANSIC_STYLE = 0
+const CPP_STYLE = 1
+
+/* Globals */
+var line_number uint
+var warn_embedded_newlines = false
+var last_char_was_newline bool = false
+
+var rc *bufio.Reader
+
+func peek() byte {
+	bytes, err := rc.Peek(1)
+	if err != nil {
+		panic("error while peeking")
+	}
+	return bytes[0]
+}
+
+func ispeek(c byte) bool {
+	if c == peek() {
+		return true
+	}
+	return false
+}
+
+/*
+ * getachar - Get one character, tracking line number
+ */
+func getachar() (byte, error) {
+	c, err := rc.ReadByte()
+	if err != nil && err != io.EOF {
+		panic("error while reading a character")
+	}
+	if last_char_was_newline {
+		line_number++
+	}
+	if c == '\n' {
+		last_char_was_newline = true
+	} else {
+		last_char_was_newline = false
+	}
+	return c, err
+}
+
+func isspace(c byte) bool {
+	return c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\f'
+}
+
+/*
+ * sloc_count - Count the SLOC in the program.
+ */
+func sloc_count(stream *os.File) uint {
+	var sloc uint = 0
+	var sawchar bool = false           /* Did you see a char on this line? */
+	var mode int = NORMAL              /* NORMAL, INSTRING, or INCOMMENT */
+	var comment_type int = ANSIC_STYLE /* ANSIC_STYLE or CPP_STYLE */
+	rc = bufio.NewReader(stream)
+ 
+	/* The following implements a state machine with transitions; the
+	 * main state is "mode" and "comment_type", the transitions are
+         * triggered by characters input.
+	 */
+
+	for {
+		c, err := getachar()
+		if err == io.EOF {
+			break
+		}
+
+		if mode == NORMAL {
+			if c == '"' {
+				sawchar = true
+				mode = INSTRING
+			} else if c == '\'' {
+				/* Consume single-character 'xxxx' values */
+				sawchar = true
+				c, err = getachar()
+				if (c == '\\') {
+					c, err = getachar()
+				}
+				for {
+					c, err = getachar()
+					if (c == '\'') || (c == '\n') || (err == io.EOF) {
+						break
+					}
+				}
+			} else if (c == '/') && ispeek('*') {
+				c, err = getachar()
+				mode = INCOMMENT
+				comment_type = ANSIC_STYLE
+			} else if (c == '/') && ispeek('/') {
+				c, err = getachar()
+				mode = INCOMMENT
+				comment_type = CPP_STYLE
+			} else if (!isspace(c)) {
+				sawchar = true
+			}
+		} else if mode == INSTRING {
+			/*
+                         * We only count string lines with non-whitespace --
+			 * this is to gracefully handle syntactically invalid
+			 * programs.
+                         * You could argue that multiline strings with
+                         * whitespace are still executable and should be
+                         * counted.
+                         */
+			if !isspace(c) {
+				sawchar = true
+			}
+			if (c == '"') {
+				mode = NORMAL
+			} else if (c == '\\') && (ispeek('"') || ispeek('\\')) {
+				c, err = getachar()
+			} else if (c == '\\') && ispeek('\n') {
+				c, err = getachar()
+			} else if (c == '\n') && warn_embedded_newlines {
+                                /* We found a bare newline in a string without
+                                 * preceding backslash. */
+				log.Printf("c_count WARNING - newline in string, line %ld, file %s\n", line_number, stream.Name())
+			       /* We COULD warn & reset mode to
+			        * "Normal", but lots of code does
+			        * this, so we'll just depend on the
+			        * warning for ending the program
+				*  in a string to catch syntactically
+                                * erroneous programs.
+                                */
+			}
+		} else {  /* INCOMMENT mode */
+			if (c == '\n') && (comment_type == CPP_STYLE) {
+				mode = NORMAL
+			}
+			if (comment_type == ANSIC_STYLE) && (c == '*') && ispeek('/') {
+				c, err = getachar()
+				mode = NORMAL
+			}
+		}
+		if (c == '\n') {
+			if (sawchar) {
+				sloc++
+			}
+			sawchar = false
+		}
+	}
+	/* We're done with the file.  Handle EOF-without-EOL. */
+	if sawchar {
+		sloc++
+	}
+	sawchar = false
+	if (mode == INCOMMENT) && (comment_type == CPP_STYLE) {
+		mode = NORMAL
+	}
+
+	if (mode == INCOMMENT) {
+		log.Printf("c_count ERROR - terminated in comment in %s\n", stream.Name())
+	} else if (mode == INSTRING) {
+		log.Printf("c_count ERROR - terminated in string in %s\n", stream.Name())
+	}
+
+	return sloc
+}
+
+/*
+ * C - recognize C files and get linecounts from them.
+ */
 func C(path string) stats.SourceStat {
 	var stat stats.SourceStat
-	return stat
+	if strings.HasSuffix(path, ".c") || strings.HasSuffix(path, ".h") {
+		stat.Language = "C"
+		f, err := os.Open(path)
+		if err != nil {
+			log.Println(err)
+			return stat
+		}
+		defer f.Close()
+		line_number = 1
+		stat.SLOC = sloc_count(f)
+	}
+	return stat		
 }
